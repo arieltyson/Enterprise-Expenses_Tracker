749096716bd47ff7ab8ad1b2a3a50003249c9c4d
Ariel Tyson <ariel30190@hotmail.com>
2023-12-22 10:18:56 -0800

    Create model for expense categories

---
+++ enterprise_expenses/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expense_tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10")]
+        public string Type { get; set; } = "Expense";
+    }
+}
