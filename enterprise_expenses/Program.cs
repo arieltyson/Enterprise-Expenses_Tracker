b26560157d374113eb2a47f3ea77f32d3cc0b2bc
Ariel Tyson <ariel30190@hotmail.com>
2023-12-22 10:30:57 -0800

    Add dependency injection for database context

--- enterprise_expenses/Program.cs
+++ enterprise_expenses/Program.cs
@@ -1,9 +1,16 @@
+using enterprise_expenses.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Dependency Injection (DI)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
