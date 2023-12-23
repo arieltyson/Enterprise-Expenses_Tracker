cb4bac7732c35e56559230e176664afed8b57228
Ariel Tyson <ariel30190@hotmail.com>
2023-12-22 20:17:58 -0800

    Correct fatal error in program.cs where dependencies were injected after program was built

--- enterprise_expenses/Program.cs
+++ enterprise_expenses/Program.cs
@@ -6,11 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
 // Dependency Injection (DI)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+var app = builder.Build();
 
 // Connection string
 // Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
