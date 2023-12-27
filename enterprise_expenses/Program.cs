b6c38a8ebfcd3c45fe260fb2586b693aeeb822c1
Ariel Tyson <ariel30190@hotmail.com>
2023-12-27 12:18:32 -0800

    Upgrade app security

--- enterprise_expenses/Program.cs
+++ enterprise_expenses/Program.cs
@@ -11,13 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 // Register Syncfusion License with temporary key
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf1FpRmNGfV5yd0VFallUTndaUj0eQnxTdEZiW31dcXFQQ2FZVkN0Ww==");
-
-// Essential Studio 7-Day License Key
-// Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf1FpRmNGfV5yd0VFallUTndaUj0eQnxTdEZiW31dcXFQQ2FZVkN0Ww==
-
-// Essential Studio 30-Day License Key
-// Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x0R3xbf1x0ZFZMZVVbQXRPIiBoS35RdURhW3xfdXFcQ2NZUEJ2
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x1WmFZfVpgd19HYlZRTGY/P1ZhSXxQd0dhXn5bcXZVTmdcV0Y=");
 
 var app = builder.Build();
 
