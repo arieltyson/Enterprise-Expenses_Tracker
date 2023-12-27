0818be8e6ae3145fb84e380ed70e522ae0cb42ee
Ariel Tyson <ariel30190@hotmail.com>
2023-12-27 00:12:41 -0800

    Implement property matching to Syncfusion Donut chart followi g officail documentation

--- enterprise_expenses/Controllers/DashboardController.cs
+++ enterprise_expenses/Controllers/DashboardController.cs
@@ -7,46 +7,63 @@ namespace enterprise_expenses.Controllers
 {
     public class DashboardController : Controller
     {
+
         private readonly ApplicationDbContext _context;
-        
+
         public DashboardController(ApplicationDbContext context)
         {
             _context = context;
         }
+
         public async Task<ActionResult> Index()
         {
-            // Last 7 days
+            // Last 7 Days
 
             DateTime StartDate = DateTime.Today.AddDays(-6);
             DateTime EndDate = DateTime.Today;
 
             List<Transaction> SelectedTransactions = await _context.Transactions
-                .Include(t => t.Category)
-                .Where(t => t.Date >= StartDate && t.Date <= EndDate)
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
                 .ToListAsync();
 
             // Total Income
 
             int TotalIncome = SelectedTransactions
-                .Where(t => t.Category.Type == "Income")
-                .Sum(t => t.Amount);
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
 
             ViewBag.TotalIncome = TotalIncome.ToString("C0");
 
-            // Total Expenses
+            // Total Expense
 
-            int TotalExpenses = SelectedTransactions
-                .Where(t => t.Category.Type == "Expense")
-                .Sum(t => t.Amount);
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
 
-            ViewBag.TotalExpenses = TotalExpenses.ToString("C0");
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
 
-            // Total Balance
+            // Balance
 
-            int Balance = TotalIncome - TotalExpenses;
+            int Balance = TotalIncome - TotalExpense;
             CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
             culture.NumberFormat.CurrencyNegativePattern = 1;
+
             ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
+
+            //Donut Chart - Expense By Category
+
+            ViewBag.DonutChartData = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .GroupBy(j => j.Category.CategoryId)
+                .Select(k => new
+                {
+                    categoryTitleWithIcon = k.First().Category.Icon + " " + k.First().Category.Title,
+                    amount = k.Sum(j => j.Amount),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                })
+                .OrderByDescending(l => l.amount)
+                .ToList();
 
             return View();
         }
