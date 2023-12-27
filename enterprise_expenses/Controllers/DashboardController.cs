d1a709785a91b0832e1013be272497632a22c257
Ariel Tyson <ariel30190@hotmail.com>
2023-12-26 21:08:55 -0800

    Create Controller Dashboard and accompanying views and set it to default view for application

---
+++ enterprise_expenses/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace enterprise_expenses.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
