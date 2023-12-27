a2bb8b8bc3d9a8f71607e80fc9f30420c4f0866f
Ariel Tyson <ariel30190@hotmail.com>
2023-12-27 00:35:40 -0800

    Create grid for recent transactions and additional information

--- enterprise_expenses/Controllers/DashboardController.cs
+++ enterprise_expenses/Controllers/DashboardController.cs
@@ -108,6 +108,14 @@ namespace enterprise_expenses.Controllers
                                           expense = expense == null ? 0 : expense.expense,
                                       };
 
+            //Recent Transactions
+
+            ViewBag.RecentTransactions = await _context.Transactions
+                .Include(i => i.Category)
+                .OrderByDescending(j => j.Date)
+                .Take(5)
+                .ToListAsync();
+
             return View();
         }
     }
