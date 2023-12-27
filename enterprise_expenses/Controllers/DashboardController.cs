e2e9f5c51f86a8133b405c8cdf1ece1752aa1b7c
Ariel Tyson <ariel30190@hotmail.com>
2023-12-26 22:20:14 -0800

    Create data values for dashboard

--- enterprise_expenses/Controllers/DashboardController.cs
+++ enterprise_expenses/Controllers/DashboardController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using enterprise_expenses.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace enterprise_expenses.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            // Last 7 days
+
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(t => t.Category)
+                .Where(t => t.Date >= StartDate && t.Date <= EndDate)
+                .ToListAsync();
+
+            // Total Income
+
+            int TotalIncome = SelectedTransactions
+                .Where(t => t.Category.Type == "Income")
+                .Sum(t => t.Amount);
+
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            // Total Expenses
+
+            int TotalExpenses = SelectedTransactions
+                .Where(t => t.Category.Type == "Expense")
+                .Sum(t => t.Amount);
+
+            ViewBag.TotalExpenses = TotalExpenses.ToString("C0");
+
+            // Total Balance
+
+            int TotalBalance = TotalIncome - TotalExpenses;
+
+            ViewBag.TotalBalance = TotalBalance.ToString("C0");
+
             return View();
         }
     }
